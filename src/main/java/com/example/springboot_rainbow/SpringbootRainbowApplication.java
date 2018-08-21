c7fef63bfd83bd772ada5d45302971627e3916fa
lwrong <2277839278@qq.com>
2018-08-21 15:16:58 +0800

    Initial commit。初始环境

---
+++ src/main/java/com/example/springboot_rainbow/SpringbootRainbowApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboot_rainbow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRainbowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRainbowApplication.class, args);
+    }
+}
